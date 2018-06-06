9060eb597895e9a94122c02ffa6614ed400e85be
Arthur Dadalto <arthurpd10@gmail.com>
2018-06-06 16:49:04 -0300

    Adicionanod HashMaps em Item e Diretorio

--- Backend/src/com/mc426/Diretorio.java
+++ Backend/src/com/mc426/Diretorio.java
@@ -5,49 +5,55 @@ import java.util.HashMap;
 import java.util.List;
 
 public class Diretorio {
-	private String nome;
-	private String caminho;
-	private static HashMap<String, Diretorio> diretorioPorNome = new HashMap<String, Diretorio>();
+
+	private static HashMap<Integer, Diretorio> diretorioPorNome = new HashMap<Integer, Diretorio>();
+
+	private static int ultimoId = 1;
+
+	public static Diretorio getDiretorioPorId(int id) {
+		return diretorioPorNome.get(id);
+	}
+
+	private static int proximoId() {
+		return Diretorio.ultimoId++;
+	}
+
+	private int id;
+
 	private List<ItemCompartilhado> listaItems = new ArrayList<ItemCompartilhado>();
-	
-	public Diretorio(String nome, String caminho) throws Exception {
-		if(diretorioPorNome.containsKey(nome)) {
-			throw new Exception("Ja existe um diretorio com esse nome");
-		}
+
+	private String nome;
+
+	public Diretorio(String nome) throws Exception {
 		this.nome = nome;
-		this.caminho = caminho;
-		diretorioPorNome.put(nome, this);
+		this.id = Diretorio.proximoId();
+		diretorioPorNome.put(this.id, this);
 	}
-	
-	public static Diretorio getDiretorioPorNome(String nome) {
-		return diretorioPorNome.get(nome);
-	}
-	
-	public String getNome() {
-		return this.nome;
-	}
-	
-	public String getCaminho() {
-		return this.caminho;
-	}
-	
-	public List<ItemCompartilhado> getItems() {
-		return this.listaItems;
-	}
-	
-	public void adicionarRepo(String nome, String chaveAutenticacao, String link) {
-		Repositorio novoRepo = new Repositorio(nome, chaveAutenticacao, link);
-		listaItems.add(novoRepo);
-	}
-	
-	public void adicionarDocGoogle(String nome, String chaveAutenticacao, String link) {
-		DocumentoGoogle novoDoc = new DocumentoGoogle(nome, chaveAutenticacao, link);
-		listaItems.add(novoDoc);
-	}
-	
+
 	public void adicionarArquivo(String nome, String caminho) {
 		Arquivo novoArq = new Arquivo(nome, caminho);
 		listaItems.add(novoArq);
 	}
-}
 
+	public void adicionarDocGoogle(String nome, String chaveAutenticacao, String link) {
+		DocumentoGoogle novoDoc = new DocumentoGoogle(nome, chaveAutenticacao, link);
+		listaItems.add(novoDoc);
+	}
+
+	public void adicionarRepo(String nome, String chaveAutenticacao, String link) {
+		Repositorio novoRepo = new Repositorio(nome, chaveAutenticacao, link);
+		listaItems.add(novoRepo);
+	}
+
+	public String getCaminho() {
+		return this.caminho;
+	}
+
+	public List<ItemCompartilhado> getItems() {
+		return this.listaItems;
+	}
+
+	public String getNome() {
+		return this.nome;
+	}
+}
--- Backend/src/com/mc426/ItemCompartilhado.java
+++ Backend/src/com/mc426/ItemCompartilhado.java
@@ -3,23 +3,25 @@ package com.mc426;
 import java.util.HashMap;
 
 public abstract class ItemCompartilhado {
-	private int id;
-	private String nome;
-	private static int ultimoId = 1;
 	private static HashMap<Integer, ItemCompartilhado> itemPorId = new HashMap<Integer, ItemCompartilhado>();
+	private static int ultimoId = 1;
 
 	public static ItemCompartilhado getItemPorId(int id) {
 		return itemPorId.get(id);
 	}
 
+	private static int proximoId() {
+		return ultimoId++;
+	}
+
+	private int id;
+
+	private String nome;
+
 	public ItemCompartilhado(String nome) {
 		this.id = proximoId();
 		ItemCompartilhado.itemPorId.put(this.id, this);
 		this.nome = nome;
-	}
-
-	private static int proximoId() {
-		return ultimoId++;
 	}
 
 	public int getId() {
