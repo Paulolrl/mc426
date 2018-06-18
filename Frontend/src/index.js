3d174abc6e2557c0c14dd1a1d913ec301ef7dc8a
Pedro Alan <pealan97@gmail.com>
2018-06-17 23:45:42 -0300

    adadads

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -9,6 +9,7 @@ import './index.css';
 import AppLogin from './applogin'
 import AppEquipes from './appequipes'
 import AppCriarEquipe from './appcriarequipe'
+import AppProjetos from './appprojetos'
 // There's no special libraries or javascript layout systems, just code written for you.
 
 render((
@@ -16,6 +17,7 @@ render((
   <Switch>
       <Route exact path="/" component={AppLogin} />
       <Route exact path="/equipes" component={AppEquipes} />
+      <Route exact path="/projetos" component={AppProjetos} />
       <Route path="/equipes/nova" component={AppCriarEquipe} />
   </Switch>
 </Router>    ), document.getElementById('root'));
