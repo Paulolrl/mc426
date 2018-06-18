a2403549b1627c5462819569584bc5af78100a9c
Pedro Alan <pealan97@gmail.com>
2018-06-18 20:17:34 -0300

    Tela de criar projeto é funcional

--- Frontend/src/appcriarprojeto.js
+++ Frontend/src/appcriarprojeto.js
@@ -12,53 +12,77 @@ const apiUrl = 'http://localhost:8080/Backend/mc426';
 
 export default class AppCriarProjeto extends Component {
 
-  render() {
-    return (
-      <TelaCriarProjeto handleClick={this.handleSubmit}
-      					nomeProjeto={this.state.nomeProjeto}
-               			descricao={this.state.descricao}
-                        idEquipes={this.state.idEquipes}
-                        prazo={this.state.prazo}
-               			setNomeProjeto={this.setNomeProjeto}
-               			setDescricao={this.setDescricao}
-                        setIdEquipes={this.setIdEquipes}
-                        setPrazo={this.setPrazo}
-               			nomeUsuario={this.state.nomeUsuario}
-		/>
-    );
-  }
+    render() {
+        return (
+            <TelaCriarProjeto handleClick={this.handleSubmit}
+            nomeProjeto={this.state.nomeProjeto}
+            descricao={this.state.descricao}
+            idEquipes={this.state.idEquipes}
+            prazo={this.state.prazo}
+            setNomeProjeto={this.setNomeProjeto}
+            setDescricao={this.setDescricao}
+            setIdEquipes={this.setIdEquipes}
+            setPrazo={this.setPrazo}
+            nomeUsuario={this.state.nomeUsuario}
+            />
+        );
+    }
 
-  handleSubmit() {
+    async handleSubmit() {
+        var authorizationBasic = window.btoa(window.localStorage.getItem('usuarioADA') + ':' + window.localStorage.getItem('senhaADA'));
+        let response1 = await fetch(apiUrl + "/projetos/", {
+            method: 'POST',
+            headers: {
+                'Authorization': 'Basic ' + authorizationBasic
+            },
+            body: JSON.stringify({
+                nome: this.state.nomeProjeto,
+                descricao: this.state.descricao,
+                prazo: this.state.prazo,
+            })
+        });
+        if(this.state.idEquipes != ""){
+            let responseJson = await response1.json();
+            let response2 = await fetch(apiUrl+"/projetos/"+responseJson.id+"/equipes/",{
+                method: 'POST',
+                headers: {
+                    'Authorization':'Basic '+ authorizationBasic
+                },
+                body: JSON.stringify({
+                    equipes: this.state.idEquipes.split(/[ ,]/).filter(function(el) {return el.length != 0}).map(x => "/equipes/" + x),
+                })
+            });
+        }
+    window.location = '/projetos';
+}
 
-
-  }
-
-  setNomeProjeto(value) {
+setNomeProjeto(value) {
     this.setState({
-      nomeProjeto: value
+        nomeProjeto: value
     })
-  }
+}
 
-  setDescricao(value) {
+setDescricao(value) {
     this.setState({
-      descricao: value
+        descricao: value
     })
-  }
+}
 
-  setIdEquipes(value) {
+setIdEquipes(value) {
     this.setState({
-      idEquipes: value
+        idEquipes: value
     })
-  }
+}
 
-  setPrazo(value) {
+setPrazo(value) {
     this.setState({
-      prazo: value
+        prazo: value
     })
-  }
+}
 
-  constructor(props) {
-	super(props);
+
+constructor(props) {
+    super(props);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.setNomeProjeto = this.setNomeProjeto.bind(this);
     this.setDescricao = this.setDescricao.bind(this);
@@ -66,17 +90,17 @@ export default class AppCriarProjeto extends Component {
     this.setPrazo = this.setPrazo.bind(this);
 
     this.state = {
-    	"nomeUsuario": window.localStorage.getItem('usuarioADA'),
-    	nomeProjeto: "",
-    	descricao: "",
-    	idEquipes: "",
+        "nomeUsuario": window.localStorage.getItem('usuarioADA'),
+        nomeProjeto: "",
+        descricao: "",
+        idEquipes: "",
         prazo: "",
         nomeUsuario: ""
     };
-  }
+}
 
-  componentDidMount() {
-  	console.log(window.localStorage.getItem('usuarioADA'));
-  	this.setState({nomeUsuario: window.localStorage.getItem('usuarioADA')});
-  }
+componentDidMount() {
+    console.log(window.localStorage.getItem('usuarioADA'));
+    this.setState({nomeUsuario: window.localStorage.getItem('usuarioADA')});
+}
 };
