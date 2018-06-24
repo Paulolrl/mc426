9673f64bea76ada58982165dbdb35c60e1ae08ce
Paulo Lacerda <paulo.lacerda@watermelontecnologia.com>
2018-06-24 20:47:42 -0300

    alguns testes

--- Backend/src/com/teste/MainTest.java
+++ Backend/src/com/teste/MainTest.java
@@ -11,6 +11,12 @@ import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
 import com.sun.jersey.test.framework.JerseyTest;
 
+import org.junit.runners.MethodSorters;
+
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class MainTest extends JerseyTest {
 
     public MainTest()throws Exception {
@@ -18,11 +24,11 @@ public class MainTest extends JerseyTest {
     }
 
     @Test
-    public void testCriarUsuario() {
+    public void test1CriarUsuario() {
     	JSONObject jsonRequest = new JSONObject("{\r\n" + 
-    			"	\"nome\": \"string\",\r\n" + 
-    			"	\"usuario\": \"arthur\",\r\n" + 
-    			"\"senha\": \"12345\",\r\n" + 
+    			"	\"nome\": \"Paulo Lucas\",\r\n" + 
+    			"	\"usuario\": \"paulo\",\r\n" + 
+    			"\"senha\": \"123456\",\r\n" + 
     			"\"gerente\": \"true\"\r\n" + 
     			"}\r\n" + 
     			"");
@@ -31,13 +37,207 @@ public class MainTest extends JerseyTest {
        	JSONObject jsonExpected = new JSONObject("{\r\n" + 
        			"    \"equipes\": [],\r\n" + 
        			"    \"tarefas\": [],\r\n" + 
-       			"    \"nome\": \"string\",\r\n" + 
-       			"    \"usuario\": \"arthur\",\r\n" + 
+       			"    \"nome\": \"Paulo Lucas\",\r\n" + 
+       			"    \"usuario\": \"paulo\",\r\n" + 
        			"    \"gerente\": true\r\n" + 
        			"}\r\n" + 
        			"");
         assertEquals(jsonExpected.toString(), response.getEntity(String.class));
         assertEquals(201,response.getStatus());
     }
-
+    
+    @Test
+    public void test2CriarProjeto() {
+    	JSONObject jsonRequest = new JSONObject("{\r\n" + 
+       			"	\"nome\": \"Projeto X\",\r\n" + 
+       			"	\"descricao\": \"Projeto para dominacao mundial\",\r\n" + 
+       			"	\"prazo\": \"2018-09-15\"\r\n" + 
+       			"}\r\n" + 
+       			"");
+       	
+       	JSONObject jsonExpected = new JSONObject("{\r\n" + 
+       			"    \"dono\": \"/usuarios/paulo\",\r\n" + 
+       			"    \"equipes\": [],\r\n" + 
+       			"    \"tarefas\": [],\r\n" + 
+       			"    \"diretorio\": \"/diretorios/1\",\r\n" + 
+       			"    \"nome\": \"Projeto X\",\r\n" + 
+       			"    \"id\": 1,\r\n" + 
+       			"    \"prazo\": \"2018-09-15\",\r\n" + 
+       			"    \"descricao\": \"Projeto para dominacao mundial\"\r\n" + 
+       			"}\r\n" + 
+       			"");
+       	
+       	ClientResponse response = resource().path("projetos").header("Authorization", "Basic cGF1bG86MTIzNDU2").post(ClientResponse.class, jsonRequest.toString());
+       			
+       	assertEquals(jsonExpected.toString(), response.getEntity(String.class));
+        assertEquals(201,response.getStatus());
+        
+    }
+    
+    @Test
+    public void test3CriarEquipeErro() {
+    	JSONObject jsonRequest = new JSONObject("{\r\n" + 
+    			"	\"nome\": \"Minha Equipe\",\r\n" + 
+    			"	\"membros\": [\"/usuarios/invalido1\", \"/usuarios/paulo\"]\r\n" + 
+    			"}\r\n" + 
+    			"");
+    	
+    	ClientResponse response = resource().path("equipes").header("Authorization", "Basic cGF1bG86MTIzNDU2").post(ClientResponse.class, jsonRequest.toString());
+    	assertEquals(404,response.getStatus());
+    	
+    }
+    
+    @Test
+    public void test4CriarEquipe() {
+    	criaUsuarios();
+    	JSONObject jsonRequest = new JSONObject("{\r\n" + 
+    			"	\"nome\": \"Minha Equipe\",\r\n" + 
+    			"	\"membros\": [\"/usuarios/paulo\", \"/usuarios/pedro\"]\r\n" + 
+    			"}\r\n" + 
+    			"");
+    	ClientResponse response = resource().path("equipes").header("Authorization", "Basic cGF1bG86MTIzNDU2").post(ClientResponse.class, jsonRequest.toString());
+    	JSONObject jsonExpected = new JSONObject("{\r\n" + 
+    			"    \"dono\": \"/usuarios/paulo\",\r\n" + 
+    			"    \"membros\": [\r\n" + 
+    			"    	 \"/usuarios/paulo\",\r\n" + 
+    			"        \"/usuarios/pedro\"\r\n" + 
+    			"    ],\r\n" + 
+    			"    \"projetos\": [],\r\n" + 
+    			"    \"nome\": \"Minha Equipe\",\r\n" + 
+    			"    \"id\": 2\r\n" + 
+    			"}\r\n" + 
+    			"");
+    	
+    	assertEquals(jsonExpected.toString(), response.getEntity(String.class));
+        assertEquals(201,response.getStatus());
+    	
+    }
+    
+    @Test
+    public void test5VisulizaEquipe() {
+    	ClientResponse response = resource().path("equipes/2").header("Authorization", "Basic cGF1bG86MTIzNDU2").get(ClientResponse.class);
+    	JSONObject jsonExpected = new JSONObject("{\r\n" + 
+    			"    \"dono\": \"/usuarios/paulo\",\r\n" + 
+    			"    \"membros\": [\r\n" + 
+    			"    	 \"/usuarios/paulo\",\r\n" + 
+    			"        \"/usuarios/pedro\"\r\n" +
+    			"    ],\r\n" + 
+    			"    \"projetos\": [],\r\n" + 
+    			"    \"nome\": \"Minha Equipe\",\r\n" + 
+    			"    \"id\": 2\r\n" + 
+    			"}");
+    	
+    	assertEquals(jsonExpected.toString(), response.getEntity(String.class));
+        assertEquals(200,response.getStatus());
+    }
+    
+    @Test
+    public void test6VisualizaEquipeErro() {
+    	ClientResponse response = resource().path("equipes/10").header("Authorization", "Basic cGF1bG86MTIzNDU2").get(ClientResponse.class);
+    	assertEquals(404,response.getStatus());
+    }
+    
+    @Test
+    public void test7CriarTarefa() {
+    	//Testa criar Tarefa
+    	JSONObject jsonRequest = new JSONObject("{\r\n" + 
+    			"	\"nome\" : \"fazer BD\",\r\n" + 
+    			"	\"descricao\": \"trabalho\",\r\n" + 
+    			"	\"prazo\": \"2018-06-08\",\r\n" + 
+    			"	\"responsaveis\": [\"/usuarios/paulo\",\"/usuarios/pedro\"],\r\n" + 
+    			"	\"dependencias\": [],\r\n" + 
+    			"	\"tags\": [\"urgente\",\"pra agora\"],\r\n" + 
+    			"	\"arquivos\": []\r\n" + 
+    			"}\r\n" + 
+    			"");
+    	ClientResponse response = resource().path("projetos/1/tarefas").header("Authorization", "Basic cGF1bG86MTIzNDU2").post(ClientResponse.class, jsonRequest.toString());
+    	JSONObject jsonExpected = new JSONObject("{\r\n" + 
+        		"    \"progresso\": {\r\n" + 
+        		"        \"texto\": \"Em espera\",\r\n" + 
+        		"        \"porcentagem\": 0\r\n" + 
+        		"    },\r\n" + 
+        		"    \"feedbacks\": [],\r\n" + 
+        		"    \"responsaveis\": [\r\n" + 
+        		"        \"/usuarios/paulo\",\r\n" + 
+        		"        \"/usuarios/pedro\"\r\n" + 
+        		"    ],\r\n" + 
+        		"    \"nome\": \"fazer BD\",\r\n" + 
+        		"    \"id\": 1,\r\n" + 
+        		"    \"prazo\": \"2018-06-08\",\r\n" + 
+        		"    \"dependencias\": [],\r\n" + 
+        		"    \"descricao\": \"trabalho\",\r\n" + 
+        		"    \"tags\": [\r\n" + 
+        		"        \"urgente\",\r\n" + 
+        		"        \"pra agora\"\r\n" + 
+        		"    ]\r\n" + 
+        		"}\r\n" + 
+        		"");
+    	assertEquals(jsonExpected.toString(), response.getEntity(String.class));
+       	assertEquals(201,response.getStatus());
+    	
+    }
+    
+    @Test
+    public void test8CriarTarefaErro() {
+    	//Testa criar Tarefa
+    	JSONObject jsonRequest = new JSONObject("{\r\n" + 
+    			"	\"nome\" : \"fazer BD\",\r\n" + 
+    			"	\"descricao\": \"trabalho\",\r\n" + 
+    			"	\"prazo\": \"2018-06-08\",\r\n" + 
+    			"	\"responsaveis\": [\"/usuarios/invalido\",\"/usuarios/pedro\"],\r\n" + 
+    			"	\"dependencias\": [],\r\n" + 
+    			"	\"tags\": [\"urgente\",\"pra agora\"],\r\n" + 
+    			"	\"arquivos\": []\r\n" + 
+    			"}\r\n" + 
+    			"");
+    	ClientResponse response = resource().path("projetos/1/tarefas").header("Authorization", "Basic cGF1bG86MTIzNDU2").post(ClientResponse.class, jsonRequest.toString());
+       	assertEquals(404,response.getStatus());
+    	
+    }
+    
+    @Test
+    public void test9VisualizarTarefa() {
+    	ClientResponse response = resource().path("projetos/1/tarefas/1").header("Authorization", "Basic cGF1bG86MTIzNDU2").get(ClientResponse.class);
+    	JSONObject jsonExpected = new JSONObject("{\r\n" + 
+        		"    \"progresso\": {\r\n" + 
+        		"        \"texto\": \"Em espera\",\r\n" + 
+        		"        \"porcentagem\": 0\r\n" + 
+        		"    },\r\n" + 
+        		"    \"feedbacks\": [],\r\n" + 
+        		"    \"responsaveis\": [\r\n" + 
+        		"        \"/usuarios/paulo\",\r\n" + 
+        		"        \"/usuarios/pedro\"\r\n" + 
+        		"    ],\r\n" + 
+        		"    \"nome\": \"fazer BD\",\r\n" + 
+        		"    \"id\": 1,\r\n" + 
+        		"    \"prazo\": \"2018-06-08\",\r\n" + 
+        		"    \"dependencias\": [],\r\n" + 
+        		"    \"descricao\": \"trabalho\",\r\n" + 
+        		"    \"tags\": [\r\n" + 
+        		"        \"urgente\",\r\n" + 
+        		"        \"pra agora\"\r\n" + 
+        		"    ]\r\n" + 
+        		"}\r\n" + 
+        		"");
+    	assertEquals(jsonExpected.toString(), response.getEntity(String.class));
+       	assertEquals(200,response.getStatus());
+    }
+    
+    @Test
+    public void test91VisualizarTarefaErro() {
+    	ClientResponse response = resource().path("projetos/1/tarefas/10").header("Authorization", "Basic cGF1bG86MTIzNDU2").get(ClientResponse.class);
+    	assertEquals(404,response.getStatus());
+    }
+    
+    public void criaUsuarios() {
+    	JSONObject jsonRequest = new JSONObject("{\r\n" + 
+    			"	\"nome\": \"Pedro Alan\",\r\n" + 
+    			"	\"usuario\": \"pedro\",\r\n" + 
+    			"	\"senha\": \"123456\",\r\n" + 
+    			"	\"gerente\": \"true\"\r\n" + 
+    			"}\r\n" + 
+    			"");
+       	resource().path("usuarios").post(ClientResponse.class,jsonRequest.toString());
+    }
+    
 }
