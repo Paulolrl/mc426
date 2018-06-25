04561faa5ac129744a96a07df0b32259447c2cfe
Arthur Dadalto <arthurpd10@gmail.com>
2018-06-25 00:11:56 -0300

    Adicionando cadastro e mensagens de erro no login

--- Frontend/src/applogin.js
+++ Frontend/src/applogin.js
@@ -8,6 +8,8 @@ import './index.css';
 // Pagedraw generates the JSX and CSS files you need.
 import TelaLogin from './pagedraw/tela_login'
 
+const apiUrl = 'http://localhost:8080/Backend/mc426';
+
 export default class AppLogin extends Component {
 
   render() {
@@ -17,14 +19,40 @@ export default class AppLogin extends Component {
                  senha={this.state.senha}
                  setUsuario={this.setUsuario}
                  setSenha={this.setSenha}
+                 fonteErro={this.state.fonteErro}
+                 usuarioCadastro={this.state.usuarioCadastro}
+                 nomeCadastro={this.state.nomeCadastro}
+                 senhaCadastro={this.state.senhaCadastro}
+                 boolGerente={this.state.boolGerente}
+                 handleClickCadastro={this.handleCadastro}
+                 setNomeCadastro={this.setNomeCadastro}
+                 setUsuarioCadastro={this.setUsuarioCadastro}
+                 setSenhaCadastro={this.setSenhaCadastro}
+                 setBoolGerente={this.setBoolGerente}
+
                  />
     );
   }
 
-  handleLogin() {
-    window.localStorage.setItem('usuarioADA', this.state.usuario);
-    window.localStorage.setItem('senhaADA', this.state.senha);
-    window.location = '/equipes';
+
+  async handleLogin() {
+    let response = await fetch(apiUrl + "/usuarios/" + this.state.usuario, {
+            method: 'GET',
+            headers: {
+                'Authorization': 'Basic ' + window.btoa(this.state.usuario + ':' + this.state.senha)
+            }
+        });
+    if (response.ok)
+    {
+      window.localStorage.setItem('usuarioADA', this.state.usuario);
+      window.localStorage.setItem('senhaADA', this.state.senha);
+      window.location = '/equipes';
+    }
+    else
+    {
+      this.setState({fonteErro: 18});
+      setTimeout(() => this.setState({fonteErro: 0}), 2000);
+    }
   }
 
   setUsuario(value) {
@@ -39,14 +67,73 @@ export default class AppLogin extends Component {
     })
   }
 
+  setNomeCadastro(value) {
+    this.setState({
+      nomeCadastro: value
+    })
+  }
+
+  setUsuarioCadastro(value) {
+    this.setState({
+      usuarioCadastro: value
+    })
+  }
+
+  setSenhaCadastro(value) {
+    this.setState({
+      senhaCadastro: value
+    })
+  }
+
+  setBoolGerente(value) {
+    this.setState({
+      boolGerente: !this.state.boolGerente
+    })
+  }
+
+
+  async handleCadastro() {
+    let response = await fetch(apiUrl + "/usuarios", {
+            method: 'POST',
+            body: JSON.stringify({
+              "nome": this.state.nomeCadastro,
+              "usuario": this.state.usuarioCadastro,
+              "senha": this.state.senhaCadastro,
+              "gerente": this.state.boolGerente
+            })
+        });
+    console.log(this.state.boolGerente);
+    if (response.ok)
+    {
+      window.localStorage.setItem('usuarioADA', this.state.usuarioCadastro);
+      window.localStorage.setItem('senhaADA', this.state.senhaCadastro);
+      window.location = '/equipes';
+    }
+    else
+    {
+      console.log("Cadastro deu errado.");
+    }
+  }
+
   constructor() {
     super();
     this.handleLogin = this.handleLogin.bind(this);
     this.setUsuario = this.setUsuario.bind(this);
     this.setSenha = this.setSenha.bind(this);
+
+    this.handleCadastro = this.handleCadastro.bind(this);
+    this.setNomeCadastro = this.setNomeCadastro.bind(this);
+    this.setUsuarioCadastro = this.setUsuarioCadastro.bind(this);
+    this.setSenhaCadastro = this.setSenhaCadastro.bind(this);
+    this.setBoolGerente = this.setBoolGerente.bind(this);
     this.state = {
       usuario: '',
-      senha: ''
+      senha: '',
+      fonteErro: 0,
+      usuarioCadastro: "",
+      nomeCadastro: "",
+      senhaCadastro: "",
+      boolGerente: false
     };
   }
 
--- Frontend/src/pagedraw/tela_login.js
+++ Frontend/src/pagedraw/tela_login.js
@@ -8,35 +8,108 @@ function render() {
         <div className="tela_login-0">
             <div className="tela_login-rectangle-6">
                 <div className="tela_login-0-0-0">
-                    <div className="tela_login-rectangle_5">
+                    <div className="tela_login-0-0-0-0">
                         <div className="tela_login-0-0-0-0-0">
-                            <div className="tela_login-0-0-0-0-0-0">
+                            <div className="tela_login-rectangle_5">
                                 <div className="tela_login-0-0-0-0-0-0-0">
-                                    <div className="tela_login-text-3">{"Usuário:"}</div>
+                                    <div className="tela_login-0-0-0-0-0-0-0-0">
+                                        <div className="tela_login-0-0-0-0-0-0-0-0-0">
+                                            <div className="tela_login-text-3">{"Usuário:"}</div>
+                                        </div>
+                                    </div>
+                                    <input type="text" placeholder="Digite usuário" value={this.props.usuario} onChange={(e) => this.props.setUsuario(e.target.value)} className="tela_login-text_input-8" /> 
                                 </div>
                             </div>
-                            <input type="text" placeholder="Digite usuário" value={this.props.usuario} onChange={(e) => this.props.setUsuario(e.target.value)} className="tela_login-text_input-8" /> 
                         </div>
-                    </div>
-                </div>
-                <div className="tela_login-0-0-1">
-                    <div className="tela_login-rectangle_6">
-                        <div className="tela_login-0-0-1-0-0">
-                            <div className="tela_login-0-0-1-0-0-0">
-                                <div className="tela_login-0-0-1-0-0-0-0">
-                                    <div className="tela_login-text-9">{"Senha:"}</div>
+                        <div className="tela_login-0-0-0-0-1">
+                            <div className="tela_login-rectangle_6">
+                                <div className="tela_login-0-0-0-0-1-0-0">
+                                    <div className="tela_login-0-0-0-0-1-0-0-0">
+                                        <div className="tela_login-0-0-0-0-1-0-0-0-0">
+                                            <div className="tela_login-text-9">{"Senha:"}</div>
+                                        </div>
+                                    </div>
+                                    <input type="password" placeholder="Digite sua senha" value={this.props.senha} onChange={(e) => this.props.setSenha(e.target.value)} className="tela_login-text_input-0" /> 
                                 </div>
                             </div>
-                            <input type="password" placeholder="Digite sua senha" value={this.props.senha} onChange={(e) => this.props.setSenha(e.target.value)} className="tela_login-text_input-0" /> 
+                        </div>
+                        <div className="tela_login-0-0-0-0-2">
+                            <div onClick={this.props.handleClick} className="tela_login-rectangle_2">
+                                <div className="tela_login-0-0-0-0-2-0-0">
+                                    <div className="tela_login-rectangle_3">
+                                        <div className="tela_login-0-0-0-0-2-0-0-0-0">
+                                            <div className="tela_login-text-36">Login</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="tela_login-0-0-0-0-2-1">
+                                <div className="tela_login-0-0-0-0-2-1-0">
+                                    <div className="tela_login-text_3" style={{"fontSize": this.props.fonteErro}}>
+                                        {"Usuário ou senha incorretos"}
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className="tela_login-0-0-2">
-                    <div onClick={this.props.handleClick} className="tela_login-rectangle_2">
-                        <div className="tela_login-0-0-2-0-0">
-                            <div className="tela_login-rectangle_3">
-                                <div className="tela_login-0-0-2-0-0-0-0">
-                                    <div className="tela_login-text-36">Login</div>
+                    <div className="tela_login-line_6" /> 
+                    <div className="tela_login-0-0-0-2">
+                        <div className="tela_login-0-0-0-2-0">
+                            <div className="tela_login-rectangle_5-3">
+                                <div className="tela_login-0-0-0-2-0-0-0">
+                                    <div className="tela_login-0-0-0-2-0-0-0-0">
+                                        <div className="tela_login-0-0-0-2-0-0-0-0-0">
+                                            <div className="tela_login-text-7">Nome</div>
+                                        </div>
+                                    </div>
+                                    <input type="text" placeholder="Digite seu nome" value={this.props.nomeCadastro} onChange={(e) => this.props.setNomeCadastro(e.target.value)} className="tela_login-text_input-1" /> 
+                                </div>
+                            </div>
+                        </div>
+                        <div className="tela_login-0-0-0-2-1">
+                            <div className="tela_login-rectangle_5-7">
+                                <div className="tela_login-0-0-0-2-1-0-0">
+                                    <div className="tela_login-0-0-0-2-1-0-0-0">
+                                        <div className="tela_login-0-0-0-2-1-0-0-0-0">
+                                            <div className="tela_login-text-362">{"Usuário:"}</div>
+                                        </div>
+                                    </div>
+                                    <input type="text" placeholder="Digite usuário" value={this.props.usuarioCadastro} onChange={(e) => this.props.setUsuarioCadastro(e.target.value)} className="tela_login-text_input-4" /> 
+                                </div>
+                            </div>
+                        </div>
+                        <div className="tela_login-0-0-0-2-2">
+                            <div className="tela_login-rectangle_6-8">
+                                <div className="tela_login-0-0-0-2-2-0-0">
+                                    <div className="tela_login-0-0-0-2-2-0-0-0">
+                                        <div className="tela_login-0-0-0-2-2-0-0-0-0">
+                                            <div className="tela_login-text-75">{"Senha:"}</div>
+                                        </div>
+                                    </div>
+                                    <input type="password" placeholder="Digite sua senha" value={this.props.senhaCadastro} onChange={(e) => this.props.setSenhaCadastro(e.target.value)} className="tela_login-text_input-88" /> 
+                                </div>
+                            </div>
+                        </div>
+                        <div className="tela_login-0-0-0-2-3">
+                            <div onClick={this.props.handleClickCadastro} className="tela_login-rectangle_2-4">
+                                <div className="tela_login-0-0-0-2-3-0-0">
+                                    <div className="tela_login-rectangle_3-0">
+                                        <div className="tela_login-0-0-0-2-3-0-0-0-0">
+                                            <div className="tela_login-text-5">Cadastro</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="tela_login-0-0-0-2-3-1">
+                                <div className="tela_login-0-0-0-2-3-1-0">
+                                    <div className="tela_login-text-76">{"Gerente?"}</div>
+                                </div>
+                            </div>
+                            <div className="tela_login-0-0-0-2-3-2">
+                                <div className="tela_login-0-0-0-2-3-2-0">
+                                    <div onClick={(e) => this.props.setBoolGerente(e.target.value)} className="tela_login-check_box-0">
+                                        <input type="checkbox" checked={(this.props.boolGerente) ? 'true' : undefined} className="tela_login-0-0-0-2-3-2-0-0-0" /> 
+                                    </div>
                                 </div>
                             </div>
                         </div>
